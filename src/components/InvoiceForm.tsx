5db5d7880dfc78bb6d88271acb0b1da178f5acdd
spaceInvaders007 <ignaciosenese@gmail.com>
2025-09-19 13:59:29 +0930

    wrapping in form and building the payload

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -2,10 +2,11 @@
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Dayjs } from "dayjs";
-import { useState } from "react";
+import { FormEvent, useCallback, useState } from "react";
 import { CurrencySelector } from "./CurrencySelector";
 import { InvoiceLines } from "./InvoiceLines";
 import { InvoiceLine } from "@/types/types";
+import { Box, Button, Stack } from "@mui/material";
 export const InvoiceForm = () => {
   const [date, setDate] = useState<Dayjs | null>(null);
   const [baseCurrency, setBaseCurrency] = useState<string | null>(null);
@@ -16,17 +17,38 @@ export const InvoiceForm = () => {
       amount: 0,
     },
   ]);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+      const finalInvoice = {
+        invoice: {
+          currency: baseCurrency,
+          date,
+          lines: [...invoiceLines],
+        },
+      };
+      console.log(finalInvoice, "final invoice ");
+    },
+    [baseCurrency, date, invoiceLines]
+  );
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker value={date} onChange={(newDate) => setDate(newDate)} />
-      <CurrencySelector
-        baseCurrency={baseCurrency}
-        setBaseCurrency={setBaseCurrency}
-      />
-      <InvoiceLines
-        invoiceLines={invoiceLines}
-        setInvoiceLines={setInvoiceLines}
-      />
+      <Box component="form" onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <DatePicker value={date} onChange={(newDate) => setDate(newDate)} />
+          <CurrencySelector
+            baseCurrency={baseCurrency}
+            setBaseCurrency={setBaseCurrency}
+          />
+          <InvoiceLines
+            invoiceLines={invoiceLines}
+            setInvoiceLines={setInvoiceLines}
+          />
+          <Button type="submit">Submit Invoice</Button>
+        </Stack>
+      </Box>
     </LocalizationProvider>
   );
 };
