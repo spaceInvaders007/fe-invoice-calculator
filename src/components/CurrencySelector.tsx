0db3ce50aa71145979258b3c4dcbfd2e2930e447
spaceInvaders007 <ignaciosenese@gmail.com>
2025-09-19 10:41:35 +0930

    Adding Currency Selector

---
+++ src/components/CurrencySelector.tsx
@@ -0,0 +1,22 @@
+import { Dispatch, SetStateAction } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import { currencies } from "@/constants/currencies";
+import TextField from "@mui/material/TextField";
+
+interface Props {
+  baseCurrency: string | null;
+  setBaseCurrency: Dispatch<SetStateAction<string | null>>;
+}
+
+export const CurrencySelector = ({ baseCurrency, setBaseCurrency }: Props) => {
+  return (
+    <Autocomplete
+      value={baseCurrency}
+      options={currencies}
+      renderInput={(params) => <TextField {...params} label="Currency" />}
+      onChange={(event: unknown, newValue: string | null) =>
+        setBaseCurrency(newValue)
+      }
+    />
+  );
+};
--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -3,12 +3,19 @@ import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Dayjs } from "dayjs";
 import { useState } from "react";
+import { CurrencySelector } from "./CurrencySelector";
 
 export const InvoiceForm = () => {
   const [date, setDate] = useState<Dayjs | null>(null);
+  const [baseCurrency, setBaseCurrency] = useState<string | null>(null);
+  
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker value={date} onChange={(newDate) => setDate(newDate)} />
+      <CurrencySelector
+        baseCurrency={baseCurrency}
+        setBaseCurrency={setBaseCurrency}
+      />
     </LocalizationProvider>
   );
 };
